f767f98f2c9f8942ea4408a51be9a1cd5264ecb7
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-09-30 20:31:08 +0700

    import and using router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Home, Promo, Splash } from './screens';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Router from './router';
 
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
-        <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
-        <Stack.Screen name="Promo" component={Promo} options={{headerShown: false}} />
-      </Stack.Navigator>
+      <Router />      
     </NavigationContainer>
   )
 }
