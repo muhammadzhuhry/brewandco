4d2b4a3c0a8b0e952227ea9d6d551677af111c8d
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-09-30 16:14:30 +0700

    create utils color

---
+++ src/utils/color.js
@@ -0,0 +1,11 @@
+export const COLOR = {
+  white: '#FFFFFF',
+  whiteBlue: '#F7F8FB',
+  whiteCream: '#FFFAF6',
+  whiteGray: '#D8D8D8',
+  grayBlue: '#C1C7D0',
+  grayBrown: '#AAAAAA',
+  semiBlack: '#181D2D',
+  midnightBlack: '#001833',
+  midnightBlue: '#324A59'
+}
