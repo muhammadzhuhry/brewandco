96e8235df022c15248df5d1f8998e773f4d3f2c5
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-09-30 22:40:32 +0700

    add button next

--- src/screens/Onboarding/index.js
+++ src/screens/Onboarding/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
-import { Logo, OnboardingImg } from '../../assets'
-import { COLOR } from '../../utils'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { OnboardingImg, IconArrowRight } from '../../assets'
+import { COLOR, SIZE } from '../../utils'
 
 const Onboarding = () => {
   return (
     <View style={styles.container}>
-      <Image source={OnboardingImg} style={styles.image} resizeMode="contain" />
-      <View style={styles.text}>
-        <Text style={styles.quote}>{`Making your days with \nour coffee.`}</Text>
-        <Text style={styles.tagline}>{`The best grain, the finest roast, the \nmost powerful flavor.`}</Text>
+      <View style={styles.wrapperHeader}>
+        <Image source={OnboardingImg} style={styles.image} resizeMode="contain" />
+        <View style={styles.text}>
+          <Text style={styles.quote}>{`Making your days with \nour coffee.`}</Text>
+          <Text style={styles.tagline}>{`The best grain, the finest roast, the \nmost powerful flavor.`}</Text>
+        </View>
+      </View>
+      {/* build in Button cannot be customized */}
+      <View style={styles.wrapperButton}>
+        <TouchableOpacity style={styles.button}> 
+          <IconArrowRight />
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -20,9 +28,12 @@ export default Onboarding
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: COLOR.white
+  },
+  wrapperHeader: {
+    marginTop: SIZE.height * 0.09, // hardcode 60px
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white'
+    justifyContent: 'center'
   },
   image: {
     width: 330,
@@ -43,5 +54,18 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins-Regular',
     color: COLOR.grayBrown,
     textAlign: 'center'
+  },
+  wrapperButton: {
+    marginTop: 50,
+    alignItems: 'flex-end',
+  },
+  button: {
+    width: 65,
+    height: 65,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: SIZE.width * 0.12, // hardcode 50px
+    backgroundColor: COLOR.midnightBlue
   }
 })
