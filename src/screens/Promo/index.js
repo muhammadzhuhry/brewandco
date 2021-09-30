0d7813626f1ff37abed34b13488e2248b467dcd3
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-09-30 16:28:47 +0700

    create screen Promo

---
+++ src/screens/Promo/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Promo = () => {
+  return (
+    <View>
+      <Text>Promo Screen</Text>
+    </View>
+  )
+}
+
+export default Promo
+
+const styles = StyleSheet.create({})
