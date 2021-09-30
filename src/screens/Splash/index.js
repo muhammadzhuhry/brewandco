ab5261da92dc5fbd8dd746a20beda07ee295804b
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-09-30 21:14:24 +0700

    add logic change splashscreen in 5S

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import hexToRgba from 'hex-to-rgba';
 import { COLOR, SIZE } from '../../utils';
 import { Logo, SplashBg } from '../../assets';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
-const Splash = () => {
+const Splash = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('Onboarding')
+    }, 5000)
+  }, [])
+
   return (
     <ImageBackground source={SplashBg} style={styles.container}>
       <View style={styles.overlay} />
       <Image source={Logo} style={styles.logo} />
       <Text style={styles.quote}>Ordinary Coffee House</Text>
     </ImageBackground>
-)
+  )
 }
 
 export default Splash
@@ -37,6 +43,6 @@ const styles = StyleSheet.create({
     marginTop: 25,
     color: COLOR.whiteCream, 
     fontSize: 24,
-    fontFamily: 'Poppins-Regular'
+    fontFamily: 'Poppins-Medium'
   }
 })
