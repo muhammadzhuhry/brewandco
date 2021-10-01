98528293a375bc990930b330b8078badbc9bb600
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-01 12:31:11 +0700

    register screen Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Home, Onboarding, Promo, Splash } from '../screens';
+import { Home, Login, Onboarding, Promo, Splash } from '../screens';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
@@ -9,6 +9,7 @@ const Router = () => {
     <Stack.Navigator initialRouteName="Splash">
       <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
       <Stack.Screen name="Onboarding" component={Onboarding} options={{headerShown: false}} />
+      <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
       <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
       <Stack.Screen name="Promo" component={Promo} options={{headerShown: false}} />
     </Stack.Navigator>
