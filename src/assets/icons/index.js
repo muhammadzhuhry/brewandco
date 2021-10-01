0b5f9045e608370cec4f9ea0d36f5878715db06c
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-01 20:25:10 +0700

    export new icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,7 @@
+import IconArrowLeft from './arrowLeft.svg';
 import IconArrowRight from './arrowRight.svg';
+import IconLock from './lock.svg';
+import IconMessage from './message.svg';
+import IconShow from './show.svg';
 
-export { IconArrowRight };
+export { IconArrowLeft, IconArrowRight, IconLock, IconMessage, IconShow };
