4e535567876b6b262f45d192542214a84d46a7bb
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-01 11:35:06 +0700

    create component ButtonIcon

---
+++ src/components/ButtonIcon/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import { IconArrowRight } from '../../assets'
+import { COLOR } from '../../utils'
+
+const ButtonIcon = ({ width, height, bradius, color, iconName }) => {
+  const Icon = () => {
+    if (iconName == 'arrow-right') return <IconArrowRight />
+
+    return null;
+  }
+  
+  return (
+    <TouchableOpacity style={styles.button(width, height, bradius, color)}> 
+      <Icon />      
+    </TouchableOpacity>
+  )
+}
+
+export default ButtonIcon
+
+const styles = StyleSheet.create({
+  button: (width, height, bradius, color) => ({
+    width: width ? width : 65,
+    height: height ? height : 65,
+    borderRadius: bradius ? bradius : 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: color ? color : COLOR.midnightBlue
+  })
+})
