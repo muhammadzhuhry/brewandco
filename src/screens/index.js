90bc0b8d482b7f9033ba93586f1ddaa042e791ab
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-03 12:30:03 +0700

    export screen Register

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,6 +2,7 @@ import Home from './Home';
 import Login from './Login';
 import Onboarding from './Onboarding';
 import Promo from './Promo';
+import Register from './Register';
 import Splash from './Splash';
 
-export { Home, Login, Onboarding, Promo, Splash };
+export { Home, Login, Onboarding, Promo, Register, Splash };
