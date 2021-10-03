75edc4af2b7aaefdc7daa1acf41dedbfd70da304
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-03 09:59:51 +0700

    create component Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import { StyleSheet, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { COLOR } from '../../utils'
 
-const Button = ({ width, height, bradius, color, onPress }) => {
+const Button = ({ color, bRadius, pVertical, mBottom, onPress, text }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={styles.button(width, height, bradius, color)} />
+    <TouchableOpacity onPress={onPress} style={styles.button(color, bRadius, pVertical, mBottom)}>
+      <Text style={styles.buttonText}>{ text ? text : "Button" }</Text>
+    </TouchableOpacity>
   )
 }
 
 export default Button
 
 const styles = StyleSheet.create({
-  button: (width, height, bradius, color) => ({
-    width: width ? width : 65,
-    height: height ? height : 65,
-    borderRadius: bradius ? bradius : 0,
-    alignItems: 'center',
-    justifyContent: 'center',
+  button: (color, bRadius, pVertical, mBottom) => ({
+    borderRadius: bRadius ? bRadius : 0,
+    paddingVertical: pVertical ? pVertical : 0,
+    marginBottom: mBottom ? mBottom : 0,
     backgroundColor: color ? color : COLOR.midnightBlue
-  })
+  }),
+  buttonText: {
+    fontSize: 14,
+    textAlign: 'center',
+    fontFamily: 'Poppins-SemiBold',
+    color: COLOR.white
+  }
 })
