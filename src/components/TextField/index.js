8682e079a9b7bda06830cb187ffa70e7ad619c1d
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-03 11:51:08 +0700

    create component TextField

---
+++ src/components/TextField/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { StyleSheet, TextInput, View } from 'react-native'
+import VerticalLine from '../VerticalLine'
+import { IconMessage } from '../../assets'
+import { COLOR } from '../../utils'
+
+const TextField = ({ iconName, placeholder, value, onChangeText }) => {
+  const Icon = () => {
+    switch (iconName) {
+      case 'message':
+        return <IconMessage />
+
+      default: 
+        null
+    }
+    return null;
+  }
+
+  return (
+    <View style={styles.input}>
+      <Icon />
+      <VerticalLine width={1} height="60%" mRight={0} mLeft={10} color={COLOR.grayBlue} />
+      <TextInput
+        value={value} 
+        placeholder={placeholder}
+        onChangeText={onChangeText} 
+      />
+    </View>
+  )
+}
+
+export default TextField
+
+const styles = StyleSheet.create({
+  input: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: COLOR.grayBlue,
+    marginBottom: 20
+  }
+})
