54515996c865c2966bc8b90a3157964833cd2c72
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-03 11:51:26 +0700

    modify component VerticalLine

--- src/components/VerticalLine/index.js
+++ src/components/VerticalLine/index.js
@@ -2,19 +2,20 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { COLOR } from '../../utils'
 
-const VerticalLine = ({ width, height, color, marginRight }) => {
+const VerticalLine = ({ width, height, color, mRight, mLeft }) => {
   return (
-    <View style={styles.verticalLine(width, height, color, marginRight)} />
+    <View style={styles.verticalLine(width, height, color, mRight, mLeft)} />
   )
 }
 
 export default VerticalLine
 
 const styles = StyleSheet.create({
-  verticalLine: (width, height, color, marginRight) => ({
+  verticalLine: (width, height, color, mRight, mLeft) => ({
     width: width ? width : 1,
     height: height ? height : '100%',
-    marginRight: marginRight ? marginRight : 0,
+    marginRight: mRight ? mRight : 0,
+    marginLeft: mLeft ? mLeft : 0,
     backgroundColor: color ? color : COLOR.midnightBlue
   })
 })
