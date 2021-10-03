7cc64e44d28090c045206d33f0a17ae221144ccb
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-03 13:43:44 +0700

    export IconPhone

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,6 +2,14 @@ import IconArrowLeft from './arrowLeft.svg';
 import IconArrowRight from './arrowRight.svg';
 import IconLock from './lock.svg';
 import IconMessage from './message.svg';
+import IconPhone from './smartphone.svg';
 import IconShow from './show.svg';
 
-export { IconArrowLeft, IconArrowRight, IconLock, IconMessage, IconShow };
+export { 
+  IconArrowLeft, 
+  IconArrowRight, 
+  IconLock, 
+  IconMessage,
+  IconPhone,
+  IconShow
+};
