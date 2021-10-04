ce2c0ab970be1423565ad33fa399e233e321860e
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-04 19:49:24 +0700

    export component LoyaltyCard

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Button from './Button';
 import ButtonIcon from './ButtonIcon';
+import LoyaltyCard from './LoyaltyCard';
 import PasswordField from './PasswordField';
 import TextField from './TextField';
 import VerticalLine from './VerticalLine';
 
-export { Button, ButtonIcon, PasswordField, TextField, VerticalLine };
+export { Button, ButtonIcon, LoyaltyCard, PasswordField, TextField, VerticalLine };
