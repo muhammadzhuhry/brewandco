79472c7170cc1278a0ff43c0b691fa2adb93bb1f
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-04 17:20:37 +0700

    add header

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { IconCart } from '../../assets';
+import { COLOR } from '../../utils';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.wrapperHeader}>
+        <View>
+          <Text style={styles.greeting}>Good morning</Text>
+          <Text style={styles.name}>Anderson</Text>
+        </View>
+        <TouchableOpacity>
+          <IconCart />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLOR.white,
+    paddingVertical: 30,
+    paddingHorizontal: 40 
+  }, 
+  wrapperHeader: {
+    marginVertical: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  greeting: {
+    fontSize: 14,
+    fontFamily: 'Poppins-Medium',
+    color: COLOR.grayBrown
+  },
+  name: {
+    fontSize: 18,
+    fontFamily: 'Poppins-Medium',
+    color: COLOR.midnightBlack
+  }
+})
