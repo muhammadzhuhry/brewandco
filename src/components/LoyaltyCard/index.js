d81e1c20a42bd1ac8cee192c7b56a3df0da7c87b
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-04 19:49:13 +0700

    create component LoyaltyCard

---
+++ src/components/LoyaltyCard/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { IconCoffeeActive, IconCoffee } from '../../assets'
+import { COLOR } from '../../utils';
+
+const LoyaltyCard = ({ ordered }) => {
+  let maxOrder = 8;
+
+  ordered > maxOrder ? ordered = maxOrder : ordered;
+  let itemCoffee = []
+
+  for (let i = 0; i < ordered; i++) {
+    itemCoffee.push(<IconCoffeeActive/>);
+  }
+
+  for (let j = 0; j < maxOrder - ordered; j++) {
+    itemCoffee.push(<IconCoffee/>);
+  }
+
+
+  return (
+    <View style={styles.loyaltyCard}>
+      <View style={styles.loyaltyText}>
+        <Text style={styles.text}>Loyalty card</Text>
+        <Text style={styles.text}>4 / 8</Text>
+      </View>
+      <View style={styles.loyaltyItem}>
+        { itemCoffee }
+      </View>
+    </View>
+  )
+}
+
+export default LoyaltyCard
+
+const styles = StyleSheet.create({
+  loyaltyCard: {
+    paddingVertical: 14,
+    paddingHorizontal: 25,
+    borderRadius: 12, 
+    backgroundColor: COLOR.midnightBlue
+  },
+  loyaltyText: { 
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 5
+  },
+  text: {
+    fontSize: 14,
+    fontFamily: 'Poppins-Medium',
+    color: COLOR.whiteGray
+  },
+  loyaltyItem: {
+    padding: 14,
+    marginTop: 5,
+    borderRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: COLOR.white
+  }
+})
