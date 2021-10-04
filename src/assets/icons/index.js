33cde330c3d2b0c66608c3ca65f6db91ee926355
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-04 19:07:18 +0700

    export icon Coffee and CoffeeActive

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,8 @@
 import IconArrowLeft from './arrowLeft.svg';
 import IconArrowRight from './arrowRight.svg';
 import IconCart from './cart.svg';
+import IconCoffee from './coffee.svg';
+import IconCoffeeActive from './coffeeActive.svg';
 import IconLock from './lock.svg';
 import IconMessage from './message.svg';
 import IconPhone from './smartphone.svg';
@@ -10,6 +12,8 @@ export {
   IconArrowLeft, 
   IconArrowRight,
   IconCart,
+  IconCoffee,
+  IconCoffeeActive,
   IconLock, 
   IconMessage,
   IconPhone,
