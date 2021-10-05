d51cad36746dbedd29fabaad34d9e62b278b48ab
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-05 18:35:15 +0700

    export component MenuItem

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,17 @@
 import Button from './Button';
 import ButtonIcon from './ButtonIcon';
 import LoyaltyCard from './LoyaltyCard';
+import MenuItem from './MenuItem';
 import PasswordField from './PasswordField';
 import TextField from './TextField';
 import VerticalLine from './VerticalLine';
 
-export { Button, ButtonIcon, LoyaltyCard, PasswordField, TextField, VerticalLine };
+export { 
+  Button,
+  ButtonIcon,
+  LoyaltyCard,
+  MenuItem,
+  PasswordField,
+  TextField,
+  VerticalLine
+};
