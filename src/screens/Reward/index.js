449165bf83e8df1e84744f7e86382ee7df8875c2
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-06 20:03:26 +0700

    create dummy screen Reward

---
+++ src/screens/Reward/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Reward = () => {
+  return (
+    <View>
+      <Text>Reward Screen</Text>
+    </View>
+  )
+}
+
+export default Reward
+
+const styles = StyleSheet.create({})
