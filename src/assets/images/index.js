c7d627b233d6fa32c2c40bb40cabc5783918d931
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-06 20:00:48 +0700

    export image home, order and reward

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,17 +1,23 @@
 import Americano from './americano.png';
 import Cappucino from './cappucino.png';
 import flatWhite from './flatWhite.png';
+import Home from './home.png';
 import Logo from './logo.png';
 import Mocha from './mocha.png';
 import OnboardingImg from './onboarding.png';
+import Order from './order.png';
+import Reward from './reward.png';
 import SplashBg from './splashbackground.png';
 
 export { 
   Americano,
   Cappucino,
   flatWhite,
+  Home,
   Mocha,
   Logo,
   OnboardingImg,
+  Order,
+  Reward,
   SplashBg
 };
