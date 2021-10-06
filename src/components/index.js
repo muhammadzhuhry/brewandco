b5ca7fc1e0ff29f219d00c8ec3f66918c3d0f738
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-06 20:02:31 +0700

    export component BottomNavigation & ItemNavigation

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
+import BottomNavigation from './BottomNavigation';
 import Button from './Button';
 import ButtonIcon from './ButtonIcon';
+import ItemNavigation from './ItemNavigation';
 import LoyaltyCard from './LoyaltyCard';
 import MenuItem from './MenuItem';
 import PasswordField from './PasswordField';
@@ -7,8 +9,10 @@ import TextField from './TextField';
 import VerticalLine from './VerticalLine';
 
 export { 
+  BottomNavigation,
   Button,
   ButtonIcon,
+  ItemNavigation,
   LoyaltyCard,
   MenuItem,
   PasswordField,
