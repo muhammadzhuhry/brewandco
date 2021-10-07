db58fced872c7fa0972863d4fda000fe15124f07
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-07 15:54:05 +0700

    add loyalty card and point card into screen Reward

--- src/screens/Reward/index.js
+++ src/screens/Reward/index.js
@@ -1,14 +1,38 @@
+import hexToRgba from 'hex-to-rgba';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Beans } from '../../assets';
+import { LoyaltyCard, PointCard } from '../../components';
+import { COLOR } from '../../utils';
 
 const Reward = () => {
   return (
-    <View>
-      <Text>Reward Screen</Text>
-    </View>
+    <ScrollView style={{  backgroundColor: COLOR.white }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Rewards</Text>
+        <View style={styles.card}>
+          <LoyaltyCard ordered={6} />
+          <PointCard points={2750} />
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
 export default Reward
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 30,
+    paddingHorizontal: 30
+  },
+  title: {
+    fontSize: 16,
+    fontFamily: 'Poppins-Medium',
+    color: COLOR.midnightBlack,
+    textAlign: 'center'
+  },
+  card: {
+    marginVertical: 25
+  }
+})
