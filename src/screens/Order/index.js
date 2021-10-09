d55f0d37971f954b966232fdb9a0bc85ca23c5bf
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-09 19:43:09 +0700

    add component HistoryOrder

--- src/screens/Order/index.js
+++ src/screens/Order/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { IconCoffeeActive } from '../../assets';
-import { HorizontalLine } from '../../components';
+import { HistoryOrder } from '../../components';
 import { COLOR } from '../../utils';
 
 const Order = () => {
@@ -9,22 +8,7 @@ const Order = () => {
     <ScrollView style={{ backgroundColor: COLOR.white }}>
       <View style={styles.container}>
         <Text style={styles.title}>My Order</Text>
-        <View style={styles.historyOrder}>
-          <Text style={styles.datetime}>24 June | 12:30 PM</Text>
-          <View style={styles.historyItem}>
-            <View style={styles.historyIcon}>
-              <IconCoffeeActive />
-            </View>
-            <View style={styles.item}>
-              <View>
-                <Text style={styles.name}>Americano</Text>
-                <Text style={styles.status}>now brewing ~</Text>
-              </View>
-              <Text style={styles.point}>$3.00</Text>
-            </View>
-          </View>
-        </View>
-        <HorizontalLine height={2} color={COLOR.whiteBrown} />
+        <HistoryOrder name="Americano"  date="24 June" time="12:30 PM" status={0} price={3} />
       </View>
     </ScrollView>
   )
@@ -42,43 +26,5 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins-Medium',
     color: COLOR.midnightBlack,
     textAlign: 'center'
-  },
-  historyOrder: {
-    marginTop: 30,
-    marginBottom: 20
-  },
-  datetime: {
-    fontSize: 10,
-    fontFamily: 'Poppins-Medium',
-    color: COLOR.whiteGray
-  },
-  historyItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 7
-  },
-  historyIcon: {
-    marginRight: 15
-  },
-  item: { 
-    flexDirection: 'row',
-    flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  name: {
-    fontSize: 14,
-    fontFamily: 'Poppins-Medium',
-    color: COLOR.midnightBlue
-  },
-  status: {
-    fontSize: 12,
-    fontFamily: 'Poppins-Medium',
-    color: COLOR.grayBrown
-  },
-  point: {
-    fontSize: 16,
-    fontFamily: 'Poppins-Medium',
-    color: COLOR.midnightBlue
   }
 })
