b43f37e5a406d4e04ba58a2bf88c687cedd960e1
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-25 09:41:29 +0700

    register screen Detail on router stack

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home, Login, Onboarding, Order, Reward, Register, Splash } from '../screens';
+import { Home, Login, Onboarding, Order, Reward, Register, Splash, Detail } from '../screens';
 import BottomNavigation from '../components/BottomNavigation';
 
 const Tab = createBottomTabNavigator();
@@ -47,7 +47,8 @@ const Router = () => {
       <Stack.Screen name="Onboarding" component={Onboarding} options={{headerShown: false}} />
       <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
       <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
-      <Stack.Screen name="Tabs" component={Tabs} options={{headerShown: false}}/>
+      <Stack.Screen name="Tabs" component={Tabs} options={{headerShown: false}} />
+      <Stack.Screen name="Detail" component={Detail} options={{headerShown: false}} />
     </Stack.Navigator>
   )
 }
