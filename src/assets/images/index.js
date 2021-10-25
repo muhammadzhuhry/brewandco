20ebe0c1143711566944285bd755de9e3bd0a4ae
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-25 09:44:27 +0700

    export hot & ice

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,6 +3,8 @@ import Beans from './beans.png';
 import Cappucino from './cappucino.png';
 import flatWhite from './flatWhite.png';
 import Home from './home.png';
+import Hot from './hot.png';
+import Ice from './ice.png';
 import Logo from './logo.png';
 import Mocha from './mocha.png';
 import OnboardingImg from './onboarding.png';
@@ -16,6 +18,8 @@ export {
   Cappucino,
   flatWhite,
   Home,
+  Hot,
+  Ice,
   Mocha,
   Logo,
   OnboardingImg,
