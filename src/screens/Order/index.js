bb7857eb2a3836787e08d7d42133832af9af6fcb
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-25 09:42:46 +0700

    add item history order

--- src/screens/Order/index.js
+++ src/screens/Order/index.js
@@ -8,7 +8,10 @@ const Order = () => {
     <ScrollView style={{ backgroundColor: COLOR.white }}>
       <View style={styles.container}>
         <Text style={styles.title}>My Order</Text>
-        <HistoryOrder name="Americano"  date="24 June" time="12:30 PM" status={0} price={3} />
+        <HistoryOrder name="Americano"  date="24 June" time="12:33 PM" status={0} price={3} />
+        <HistoryOrder name="Flat White"  date="24 June" time="12:31 PM" status={0} price={4.5} />
+        <HistoryOrder name="Mocha"  date="24 June" time="12:30 PM" status={0} price={4} />
+        <HistoryOrder name="Cappuccino"  date="23 June" time="07:45 AM" status={1} price={5.5} />
       </View>
     </ScrollView>
   )
