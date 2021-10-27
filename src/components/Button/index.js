68652387fbcfc745f1e38c92c5791b37e71dd7bf
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-27 17:29:30 +0700

    add parameter mTop

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { COLOR } from '../../utils'
 
-const Button = ({ color, bRadius, pVertical, mBottom, onPress, text }) => {
+const Button = ({ color, bRadius, pVertical, mBottom, mTop, onPress, text }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={styles.button(color, bRadius, pVertical, mBottom)}>
+    <TouchableOpacity onPress={onPress} style={styles.button(color, bRadius, pVertical, mBottom, mTop)}>
       <Text style={styles.buttonText}>{ text ? text : "Button" }</Text>
     </TouchableOpacity>
   )
@@ -13,10 +13,11 @@ const Button = ({ color, bRadius, pVertical, mBottom, onPress, text }) => {
 export default Button
 
 const styles = StyleSheet.create({
-  button: (color, bRadius, pVertical, mBottom) => ({
+  button: (color, bRadius, pVertical, mBottom, mTop) => ({
     borderRadius: bRadius ? bRadius : 0,
     paddingVertical: pVertical ? pVertical : 0,
     marginBottom: mBottom ? mBottom : 0,
+    marginTop: mTop ? mTop : 0,
     backgroundColor: color ? color : COLOR.midnightBlue
   }),
   buttonText: {
