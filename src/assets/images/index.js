c631574f38e674cc273f0366ff10b53fbf50007b
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-27 17:29:10 +0700

    export hot, ice, small, medium & large

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,11 +5,14 @@ import flatWhite from './flatWhite.png';
 import Home from './home.png';
 import Hot from './hot.png';
 import Ice from './ice.png';
+import Large from './large.png';
 import Logo from './logo.png';
+import Medium from './medium.png';
 import Mocha from './mocha.png';
 import OnboardingImg from './onboarding.png';
 import Order from './order.png';
 import Reward from './reward.png';
+import Small from './small.png';
 import SplashBg from './splashbackground.png';
 
 export { 
@@ -20,10 +23,13 @@ export {
   Home,
   Hot,
   Ice,
-  Mocha,
+  Large,
   Logo,
+  Medium,
+  Mocha,
   OnboardingImg,
   Order,
   Reward,
+  Small,
   SplashBg
 };
