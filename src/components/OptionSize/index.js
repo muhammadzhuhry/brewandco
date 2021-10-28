8e9abe9efe78e2a32c38ddc3030b195533ea0d82
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-28 14:09:26 +0700

    change Images to Icon SVG

--- src/components/OptionSize/index.js
+++ src/components/OptionSize/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { Large, Medium, Small } from '../../assets'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { IconLarge, IconMedium, IconSmall } from '../../assets'
 import { COLOR } from '../../utils'
 
 const OptionSize = ({ small, medium, large, onPress }) => {
@@ -9,16 +9,16 @@ const OptionSize = ({ small, medium, large, onPress }) => {
       <Text style={styles.text}>Size</Text>
       <View style={styles.item}>
         <TouchableOpacity onPress={() => onPress('small')}>
-          <Image source={Small} style={styles.option(small)} />
+          <IconSmall style={styles.option(small)} />
         </TouchableOpacity>
         <TouchableOpacity 
           onPress={() => onPress('medium')}
           style={{ marginHorizontal: 25 }}
         >
-          <Image source={Medium} style={styles.option(medium)} />
+          <IconMedium style={styles.option(medium)} />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => onPress('large')}>
-          <Image source={Large} style={styles.option(large)} />
+          <IconLarge style={styles.option(large)} />
         </TouchableOpacity>
       </View>
     </View>
