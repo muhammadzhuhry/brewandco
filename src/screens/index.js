61d977ffe2341600c3edb541a20232be7d7c9423
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-28 13:09:44 +0700

    export Screen OrderSuccess

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,8 +4,9 @@ import Home from './Home';
 import Login from './Login';
 import Onboarding from './Onboarding';
 import Order from './Order';
+import OrderSuccess from './OrderSuccess';
 import Reward from './Reward';
 import Register from './Register';
 import Splash from './Splash';
 
-export { Cart, Detail, Home, Login, Onboarding, Order, Reward, Register, Splash };
+export { Cart, Detail, Home, Login, Onboarding, Order, OrderSuccess, Reward, Register, Splash };
