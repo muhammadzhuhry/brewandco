cd55125889295d265fc59c79e2b63f18b8ce1416
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-28 14:08:59 +0700

    unexport unused Images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,16 +3,11 @@ import Beans from './beans.png';
 import Cappucino from './cappucino.png';
 import flatWhite from './flatWhite.png';
 import Home from './home.png';
-import Hot from './hot.png';
-import Ice from './ice.png';
-import Large from './large.png';
 import Logo from './logo.png';
-import Medium from './medium.png';
 import Mocha from './mocha.png';
 import OnboardingImg from './onboarding.png';
 import Order from './order.png';
 import Reward from './reward.png';
-import Small from './small.png';
 import SplashBg from './splashbackground.png';
 
 export { 
@@ -21,15 +16,10 @@ export {
   Cappucino,
   flatWhite,
   Home,
-  Hot,
-  Ice,
-  Large,
   Logo,
-  Medium,
   Mocha,
   OnboardingImg,
   Order,
   Reward,
-  Small,
   SplashBg
 };
