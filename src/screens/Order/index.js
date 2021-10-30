d8c5cc1338de29a7dc8fde330664c3088188764e
muhammadzhuhry <muhammadzhuhry@gmail.com>
2021-10-30 21:01:50 +0700

    add logic get data order history from firestore

--- src/screens/Order/index.js
+++ src/screens/Order/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { HistoryOrder } from '../../components';
 import { COLOR } from '../../utils';
 
 const Order = () => {
+  const [orders, setOrders] = useState([{}]);
+
+  let userId;
+  const refOrders = firestore().collection('orders');
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = async () => {
+    const getUserdata = await AsyncStorage.getItem('userdata')
+    userId = JSON.parse(getUserdata).id;
+
+    const query = await refOrders.where('userId', '==', userId).get();
+    let list = [];
+    query.forEach(async item => {
+      const { name, price, orderAt } = item.data();
+      list.push({
+        name,
+        price,
+        orderAt
+      })
+    });
+    await setOrders(list);
+  };
+
   return (
     <ScrollView style={{ backgroundColor: COLOR.white }}>
       <View style={styles.container}>
         <Text style={styles.title}>My Order</Text>
-        <HistoryOrder name="Americano"  date="24 June" time="12:33 PM" status={0} price={3} />
+        {
+          orders && orders.map((item, index) => {
+            return (
+              <HistoryOrder key={index} name={item.name}  date="24 June" time="12:33 PM" status={1} price={item.price} />
+            )
+          })
+        }
+        {/* <HistoryOrder name="Americano"  date="24 June" time="12:33 PM" status={0} price={3} />
         <HistoryOrder name="Flat White"  date="24 June" time="12:31 PM" status={0} price={4.5} />
         <HistoryOrder name="Mocha"  date="24 June" time="12:30 PM" status={0} price={4} />
-        <HistoryOrder name="Cappuccino"  date="23 June" time="07:45 AM" status={1} price={5.5} />
+        <HistoryOrder name="Cappuccino"  date="23 June" time="07:45 AM" status={1} price={5.5} /> */}
       </View>
     </ScrollView>
   )
